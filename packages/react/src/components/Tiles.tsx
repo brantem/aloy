86a0649f4f5327366b2ac9a32cbbf5cbd26625f2
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2024-08-29 00:36:21 +0700

    packages/react: Add dark UI for tiles

--- packages/react/src/components/Tiles.tsx
+++ packages/react/src/components/Tiles.tsx
@@ -1,10 +1,11 @@
-import { cn } from 'lib/helpers';
 import { Fragment } from 'react';
+
+import { cn } from 'lib/helpers';
 
 export default function Tiles({ n }: { n: number }) {
   return (
     <div
-      className="grid select-none grid-cols-[repeat(var(--n),500px)] grid-rows-[repeat(var(--n),500px)] text-[10rem] font-extrabold text-neutral-300 mix-blend-multiply"
+      className="grid select-none grid-cols-[repeat(var(--n),500px)] grid-rows-[repeat(var(--n),500px)] text-[10rem] font-extrabold text-neutral-300 mix-blend-multiply dark:text-neutral-700"
       style={{ '--n': n } as React.CSSProperties}
     >
       {Array.from({ length: n }).map((_, i) => (
@@ -14,7 +15,9 @@ export default function Tiles({ n }: { n: number }) {
               key={'' + i + j}
               className={cn(
                 'flex items-center justify-center',
-                (i % 2 === 0 ? j % 2 === 0 : j % 2 !== 0) && 'bg-neutral-100',
+                (i % 2 === 0 ? j % 2 === 0 : j % 2 !== 0)
+                  ? 'bg-neutral-100 dark:bg-neutral-800'
+                  : 'dark:bg-neutral-900/90',
               )}
             >{`${i},${j}`}</div>
           ))}
