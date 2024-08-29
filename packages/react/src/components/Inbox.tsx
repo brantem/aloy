c6fe5df5d19bbf2a436d2094c8a859fd48418c43
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2024-08-30 00:33:50 +0700

    packages/react: Fix <Inbox /> not scrollable

--- packages/react/src/components/Inbox.tsx
+++ packages/react/src/components/Inbox.tsx
@@ -1,5 +1,4 @@
 import { createPortal } from 'react-dom';
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
 
 import Comment from 'components/Comment';
 
@@ -7,8 +6,6 @@ import { State } from 'types';
 import { useAppStore } from 'lib/stores';
 import { cn } from 'lib/helpers';
 import { useEscape, usePins } from 'lib/hooks';
-
-// TODO: should be scrollable
 
 export default function Inbox() {
   const { nodes, activeId, setActiveId } = usePins();
@@ -31,60 +28,28 @@ export default function Inbox() {
     );
   }
 
-  const i = nodes.findIndex((pin, i) => (activeId ? pin.id === activeId : i === 0));
-  const before = i > 0 ? nodes[i - 1] : null;
-  const after = i < nodes.length - 1 ? nodes[i + 1] : null;
-
   return createPortal(
-    <div className="fixed bottom-[74px] left-0 right-0 z-[1004]">
-      <div className="relative ml-[50%] flex w-screen -translate-x-1/2 snap-x overflow-hidden pb-6 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-        {nodes.map((pin) => {
-          const isReadonly = pin.id !== activeId;
-          return (
-            <div
-              key={pin.id}
-              className="mr-4 first:ml-0 first:pl-[calc(50vw-theme(spacing.4)/2)] last:mr-0 last:pr-[calc(50vw-theme(spacing.4)/2)]"
-            >
-              <Comment
-                data-id={pin.id}
-                isRoot
-                comment={{ pin_id: pin.id, ...pin.comment, user: pin.user }}
-                className={cn(
-                  'cursor-pointer select-none snap-center rounded-lg border border-neutral-200 bg-white',
-                  isReadonly ? 'opacity-50' : 'shadow-sm',
-                )}
-                isReadonly={isReadonly}
-                showMarkAsDone
-                isCompleted={!!pin.completed_at}
-                onClick={() => setActiveId(pin.id)}
-                isFixed
-              />
-            </div>
-          );
-        })}
-      </div>
-
-      <div className="absolute left-0 right-0 top-[72px] z-[1005] mx-auto flex h-0 w-72 items-center justify-between">
-        {before ? (
-          <button
-            className="-ml-16 flex size-10 cursor-pointer items-center justify-center rounded-full border border-neutral-200 bg-white shadow-md"
-            onClick={() => setActiveId(before.id)}
-          >
-            <ChevronLeftIcon className="-ml-0.5 size-5" />
-          </button>
-        ) : (
-          <div />
-        )}
-
-        {after ? (
-          <button
-            className="-mr-16 flex size-10 cursor-pointer items-center justify-center rounded-full border border-neutral-200 bg-white shadow-md"
-            onClick={() => setActiveId(after.id)}
-          >
-            <ChevronRightIcon className="-mr-0.5 size-5" />
-          </button>
-        ) : null}
-      </div>
+    <div className="fixed bottom-[74px] left-0 right-0 z-[1004] flex snap-x gap-4 overflow-auto pb-6 pl-[calc(50vw-theme(spacing.36)-theme(spacing.4))] pr-[calc(50vw-theme(spacing.36)-theme(spacing.4))] [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+      {nodes.map((pin) => {
+        const isReadonly = pin.id !== activeId;
+        return (
+          <Comment
+            key={pin.id}
+            data-id={pin.id}
+            isRoot
+            comment={{ pin_id: pin.id, ...pin.comment, user: pin.user }}
+            className={cn(
+              'flex-shrink-0 cursor-pointer select-none snap-center rounded-lg border border-neutral-200 bg-white',
+              isReadonly ? 'opacity-50' : 'shadow-sm',
+            )}
+            isReadonly={isReadonly}
+            showMarkAsDone
+            isCompleted={!!pin.completed_at}
+            onClick={() => setActiveId(pin.id)}
+            isFixed
+          />
+        );
+      })}
     </div>,
     document.body,
   );
