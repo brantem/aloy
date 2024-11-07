9f3a5f94ed273c859ca2fd4a82aa2e0294f2a0cd
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2024-11-07 16:45:54 +0700

    packges/react: Fix import

--- packages/react/tailwind.config.js
+++ packages/react/tailwind.config.js
@@ -1,8 +1,10 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [typography],
 };
