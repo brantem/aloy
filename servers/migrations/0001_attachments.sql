a8766921f6bcb868000801d615b2f9772637c935
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2024-11-26 00:40:42 +0700

    servers/migrations: Fix typo

--- servers/migrations/0001_attachments.sql
+++ servers/migrations/0001_attachments.sql
@@ -1,7 +1,7 @@
 -- Migration number: 0001 	 2024-11-06T06:57:12.934Z
 CREATE TABLE attachments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  comment_id TEXT NOT NULL,
+  comment_id INTEGER NOT NULL,
   url TEXT NOT NULL,
   data TEXT,
   FOREIGN KEY (comment_id) REFERENCES comments(id) ON DELETE CASCADE
