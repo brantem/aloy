93e57b55a25e5a88393a40827ec8aed18262947f
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2024-12-02 16:08:50 +0700

    packages/react: Add button to move <Inbox /> to left or right side of the page

--- packages/react/src/components/Inbox.tsx
+++ packages/react/src/components/Inbox.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
+import { ChevronLeftIcon } from '@heroicons/react/20/solid';
 
 import Comment from 'components/Comment';
 
@@ -19,11 +20,10 @@ export default function Inbox() {
   }));
   useEscape(handleEscape);
 
+  const [position, setPosition] = useState<'left' | 'right'>('right');
   const [filter, setFilter] = useState<{ search: string; done: boolean | undefined }>({ search: '', done: undefined });
 
   if (!isOpen) return null;
-
-  // TODO: add button to move to the other side
 
   // TODO: api
   const pins = nodes.filter((node) => {
@@ -34,11 +34,11 @@ export default function Inbox() {
   });
 
   return createPortal(
-    <div className="fixed bottom-4 right-4 top-4 z-[1004]">
-      <div className="flex max-h-full flex-col overflow-hidden rounded-lg border border-neutral-200 bg-neutral-50 shadow-sm">
+    <div className={cn('fixed bottom-4 top-4 z-[1004]', position === 'left' ? 'left-4' : 'right-4')}>
+      <div className="relative flex max-h-full flex-col rounded-lg border border-neutral-200 bg-neutral-50 shadow-sm">
         <input
           type="text"
-          className="w-full rounded-t-lg border-b border-neutral-200 bg-white px-2 py-3 text-sm"
+          className="w-full rounded-t-lg bg-white px-2 py-3 text-sm"
           placeholder="Search..."
           value={filter.search}
           onChange={(e) => setFilter((prev) => ({ ...prev, search: e.target.value }))}
@@ -46,7 +46,7 @@ export default function Inbox() {
         />
 
         <select
-          className="border-b border-neutral-200 bg-transparent p-2 text-sm"
+          className="appearance-none border-y border-neutral-200 bg-transparent p-2 text-sm"
           onChange={(e) => {
             setFilter((prev) => ({ ...prev, done: e.target.value === '-1' ? undefined : e.target.value === '1' }));
           }}
@@ -58,25 +58,38 @@ export default function Inbox() {
           <option value="0">Unresolved</option>
         </select>
 
+        <button
+          className={cn(
+            'absolute top-[33px] z-10 flex size-6 items-center justify-center rounded-full border border-neutral-200 bg-white shadow-sm hover:bg-neutral-100',
+            position === 'left' ? '-right-3 rotate-180' : '-left-3',
+          )}
+          onClick={() => setPosition((prev) => (prev === 'left' ? 'right' : 'left'))}
+        >
+          <ChevronLeftIcon className="size-5" />
+        </button>
+
         <div className="flex flex-1 snap-y snap-mandatory flex-col gap-2 overflow-y-auto p-2 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
           {pins.length ? (
             pins.map((pin) => {
               const isSelected = pin.id === activeId;
               return (
-                <Comment
-                  key={pin.id}
-                  data-id={pin.id}
-                  isRoot
-                  isInInbox
-                  comment={{ pin_id: pin.id, ...pin.comment, user: pin.user }}
-                  className={cn(
-                    'w-72 flex-shrink-0 cursor-pointer select-none snap-start scroll-mt-2 rounded-lg border border-neutral-200 bg-white',
-                    isSelected && 'outline outline-offset-2 outline-blue-500',
-                  )}
-                  showMarkAsDone
-                  isCompleted={!!pin.completed_at}
-                  onClick={() => setActiveId(pin.id, false)}
-                />
+                <div key={pin.id} className={cn('snap-start scroll-mt-2', pin.total_replies && 'pb-[13px]')}>
+                  <Comment
+                    data-id={pin.id}
+                    isRoot
+                    isInInbox
+                    comment={{ pin_id: pin.id, ...pin.comment, user: pin.user }}
+                    className={cn(
+                      'w-72 cursor-pointer select-none rounded-lg border border-neutral-200 bg-white',
+                      isSelected && 'outline outline-offset-2 outline-blue-500',
+                    )}
+                    showMarkAsDone
+                    isCompleted={!!pin.completed_at}
+                    totalReplies={pin.total_replies}
+                    showTotalReplies
+                    onClick={() => setActiveId(pin.id, false)}
+                  />
+                </div>
               );
             })
           ) : (
